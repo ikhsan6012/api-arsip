56d84580deed559442be031758969fe348b77ccc
ikhsan6012 <ikhsan6012.com>
2019-05-12 21:57:12 +0700

    Penambahan Validasi File JSON Import SPT LB

--- app.js
+++ app.js
@@ -56,22 +56,72 @@ app.post('/upload', (req, res) => {
 app.post('/importlb', (req, res) => {
 	const file = req.files.file
 	const lb = JSON.parse(Buffer.from(file.data).toString())
+	let isValid = true
+	let customMsg = ''
+	let title = "File JSON Tidak Valid..."
+	if(!lb.length){
+		return res.json({ ok: false, message: "File JSON Tidak Valid..." })
+	}
 	const update = lb.map(l => {
-		const npwp1 = l.npwp.split('-')[0]
-		const npwp2 = l.npwp.split('-').filter((n, i) => i > 0).join('.')
-		const npwp = `${npwp1}-${npwp2}`
-		return {
-			updateOne: {
-				filter: { no_tt: l.no_tt },
-				update: { ...l, npwp },
-				upsert: true
+		if(!l.npwp) {
+			isValid = false
+			customMsg = "NPWP"
+			return false
+		}else if(!l.no_tt){
+			isValid = false
+			customMsg = "No. Tanda Terima"
+			return false
+		}else if(!l.tgl_spt){
+			isValid = false
+			customMsg = "Tanggal SPT"
+			return false
+		}else if(!l.nilai){
+			isValid = false
+			customMsg = "Nilai LB"
+			return false
+		}else if(!l.masa_tahun){
+			isValid = false
+			customMsg = "Masa/Tahun Pajak"
+			return false
+		}else if(l.masa_tahun.length === 2){
+			isValid = false
+			title = "Kemungkinan Data yang Diimport Merupakan SPT WP yang Menggunakan Mata Uang Asing."
+			customMsg = 1
+			return false
+		}else if(!l.res_kom){
+			isValid = false
+			customMsg = "Status"
+			return false
+		}else if(!l.sumber){
+			isValid = false
+			customMsg = "Sumber"
+			return false
+		}else if(!l.tgl_terima){
+			isValid = false
+			customMsg = "Tanggal Terima"
+			return false
+		}else{
+			const npwp1 = l.npwp.split('-')[0]
+			const npwp2 = l.npwp.split('-').filter((n, i) => i > 0).join('.')
+			const npwp = `${npwp1}-${npwp2}`
+			return {
+				updateOne: {
+					filter: { no_tt: l.no_tt },
+					update: { ...l, npwp },
+					upsert: true
+				}
 			}
 		}
 	})
-	LBModel.bulkWrite(update)
-		.then(() => {
-			res.json(file)
-		})
+	let message = customMsg !== 1 ? `Terdapat Data yang Tidak Memiliki ${customMsg}...` : "Jika Benar Perbaiki Masa/Tahun Pajak dan Seterusnya!!!"
+	if(!isValid){
+		res.json({ ok: false, title, message })
+	}else{
+		LBModel.bulkWrite(update)
+			.then(({ upsertedCount }) => {
+				res.json({ ok: true, upsertedCount, total: lb.length })
+			})
+	}
 })
 
 // Connect to MongoDB
